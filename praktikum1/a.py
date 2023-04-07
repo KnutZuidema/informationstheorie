ccc59dd09c6a2f07cdc864dc5552a2db9bd096db
Knut Zuidema <me@knutzuidema.de>
2023-04-07 22:35:58 +0200

    praktikum 1 aufgabe a
    rename variables and move total variable declaration

--- praktikum1/a.py
+++ praktikum1/a.py
@@ -6,24 +6,24 @@ def Z_statistik(text_file):
     with open(text_file, 'r', encoding='utf-8') as file:
         text = file.read()
 
-    # Schritt 2: Zählen der Zeichen im Text
-    char_counts = {}
+    # Schritt 2: Zählen der Häufigkeit der Zeichen im Text
+    total = len(text)
+    counts = {}
     for char in text:
-        char_counts[char] = char_counts.get(char, 0) + 1
+        counts[char] = counts.get(char, 0) + 1
 
     # Schritt 3: Berechnen der Wahrscheinlichkeiten für jedes Zeichen
-    total_chars = len(text)
-    char_probabilities = {char: count / total_chars for char, count in char_counts.items()}
+    probabilities = {char: count / total for char, count in counts.items()}
 
     # Schritt 4: Berechnen des Informationsgehalts für jedes Zeichen
-    char_information = {char: -math.log2(prob) for char, prob in char_probabilities.items()}
+    information = {char: -math.log2(prob) for char, prob in probabilities.items()}
 
     # Schritt 5: Berechnen der Entropie des Textes
-    entropy = sum(prob * info for prob, info in zip(char_probabilities.values(), char_information.values()))
+    entropy = sum(prob * info for prob, info in zip(probabilities.values(), information.values()))
 
     # Schritt 6: Ausgabe der Ergebnisse
     print("Buchstabe | Wahrscheinlichkeit | Informationsgehalt")
-    for char, prob, info in zip(char_counts.keys(), char_probabilities.values(), char_information.values()):
+    for char, prob, info in zip(counts.keys(), probabilities.values(), information.values()):
         print(f"{char!r:<9} | {prob:.6f}           | {info:.6f}")
 
     print(f"\nEntropie des Textes: {entropy:.6f}")
